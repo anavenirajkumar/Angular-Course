b24f99660931c6d6ba630e8a4b98ac734532691b
rajkumar <anaveni.laxminarsaiah123@gmail.com>
2021-12-07 07:55:29 +0530

    Reactive Form Validations

--- 15-angular-form-validations/src/app/reactive-forms/reactive-forms.component.ts
+++ 15-angular-form-validations/src/app/reactive-forms/reactive-forms.component.ts
@@ -6,27 +6,21 @@ import { FormGroup,Validators, FormControl } from '@angular/forms';
   styleUrls: ['./reactive-forms.component.css']
 })
 export class ReactiveFormsComponent implements OnInit {
-
   exform: FormGroup | any;
-  
-  // submitted = false;
-
   public courses:string[]=['html','css','javascript','bootstrap','angular','reactjs']
+  public inputType : string = 'password';
+  constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.exform = new FormGroup({
-      name: new FormControl('', [Validators.required,Validators.pattern('[a-zA-Z ]*')]),
+      name: new FormControl('', [Validators.required,Validators.minLength(5)]),
       email: new FormControl('', [Validators.required, Validators.email]),
       date : new FormControl('', [Validators.required]),
-      password : new FormControl('',[Validators.required, Validators.minLength(8)]),
-      conformpassword : new FormControl('', [Validators.required, Validators.minLength(8)]),
-      phone: new FormControl('', [
-        Validators.required,
-        Validators.pattern(
-          '^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$'
-        ),
-        Validators.maxLength(10),
-      ]),
+      password : new FormControl('',[Validators.required, Validators.minLength(8), 
+        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]),
+      conformpassword : new FormControl('', [Validators.required, Validators.minLength(5)]),
+      showPassword: new FormControl(false),
+      phone: new FormControl('', [Validators.required,Validators.maxLength(10), Validators.pattern('[6-9]\\d{9}')]),
       address: new FormControl('', [
         Validators.required,
         Validators.minLength(20),
@@ -40,6 +34,7 @@ export class ReactiveFormsComponent implements OnInit {
     });
   }
 
+
   clicksub() {
     //debugger;
         // console.log(this.exform.value);
@@ -52,52 +47,52 @@ export class ReactiveFormsComponent implements OnInit {
     // console.log(this.exform.value);
     //this.exform.reset();
   }
-  public name() {
-    return this.exform.get('name');
-  }
-  public email() {
-    return this.exform.get('email');
-  }
-  public password(){
-    return this.exform.get('password')
-  }
-  public conformpassword(){
-    return this.exform.get('conformpassword')
-  }
+  // public name() {
+  //   return this.exform.get('name');
+  // }
+  // public email() {
+  //   return this.exform.get('email');
+  // }
+  // public password(){
+  //   return this.exform.get('password')
+  // }
+  // public conformpassword(){
+  //   return this.exform.get('conformpassword')
+  // }
 
   public validatePassord(){
-    console.log(this.exform.value.password);
-    console.log(this.exform.value.conformpassword)
+    // console.log(this.exform.value.password);
+    // console.log(this.exform.value.conformpassword)
     console.log(this.exform.value.password === this.exform.value.conformpassword)
 
     return this.exform.value.password === this.exform.value.conformpassword;
   }
-  public phone() {
-    return this.exform.get('phone');
-  }
-  public address() {
-    return this.exform.get('address');
-  }
+  
+//   public phone() {
+//     return this.exform.get('phone');
+//   }
+//   public address() {
+//     return this.exform.get('address');
+//   }
 
-  public getCourse(){
-    return this.exform.get('course')
-  }
-  public date(){
-    return this.exform.get('date')
-  }
+//   public getCourse(){
+//     return this.exform.get('course')
+//   }
+//   public date(){
+//     return this.exform.get('date')
+//   }
 
-  public isTCAccepted() {
-    //console.log(this.exform.value)
-    return this.exform.get('isTCAccepted');
- }
+//   public isTCAccepted() {
+//     return this.exform.get('isTCAccepted');
+//  }
 
- public gender(){
-   return this.exform.get('gender')
- }
+//  public gender(){
+//    return this.exform.get('gender')
+//  }
 
-  // public acceptTerms(){
-  //   return this.exform.get('acceptTerms')
-  // }
+//   public acceptTerms(){
+//     return this.exform.get('acceptTerms')
+//   }
 
   public remember(e:any){
     if(e.target.checked){
@@ -111,6 +106,10 @@ export class ReactiveFormsComponent implements OnInit {
    }
   }
 
+  public showPassword(){
+    (this.inputType === 'password') ? this.inputType = 'text' : this.inputType = 'password';
+  }
+
   // public everykeyord(e:any){
   //   // this.exform.get('address2').setValue(this.exform.value.address);
   //   this.exform.get('address2').setValue(this.exform.value.address);
@@ -118,4 +117,11 @@ export class ReactiveFormsComponent implements OnInit {
   //   //this.exform.address2 = this.exform.value.address;
    
   // }
+
+  
+  public getToday(): string {
+    return new Date().toISOString().split('T')[0]
+ }
+  
+
 }
